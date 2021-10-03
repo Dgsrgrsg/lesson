fcb268703061bc8ea30ec72648ab16b1d7ba2a67
Dgsrgrsg <molchanov_diman@mail.ru>
2021-10-03 19:36:53 +0300

    “Heroku”

--- learning/settings.py
+++ learning/settings.py
@@ -16,6 +16,12 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+import django_heroku
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+django_heroku.settings(locals())
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
